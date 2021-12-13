7e68ad662f36f94b1ff8540f00a23558f91088fb
Victor Bezerra <victorbzrra@alu.ufc.br>
2021-12-13 08:37:10 -0300

    Componente TransactionTable.

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary"
+import { TransactionTable } from "../TransactionsTable"
 import { Container } from "./styles"
 
 export const Dashboard = () => {
   return(
     <Container>
       <Summary />
+      <TransactionTable />
     </Container>
   )
 }
